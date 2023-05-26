f23114748ecb221652cdc0d90ac6ef1097622fd0
George V. Reilly <george@reilly.org>
2023-05-25 22:23:58 -0700

    Update for Pelican 4.8.0

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 AUTHOR = 'George V. Reilly'
 SITENAME = 'Wild Geese Players of Seattle'
 SITEURL = ''
@@ -11,6 +7,7 @@ PATH = 'content'
 STATIC_PATHS = ['extras', 'posters', 'images', 'scripts']
 
 TIMEZONE = 'America/Los_Angeles'
+
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -21,17 +18,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
 
-# the Wild Geese Players of Seattle Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 4
-
 EXTRA_PATH_METADATA = {
     'extras/favicon.ico': {'path': 'favicon.ico'},
     'extras/CNAME': {'path': 'CNAME'},
